b47ff8dd093f73507efb2e23ff69e33b59dd8f0d
AntonWallberg <awallber@kth.se>
2025-12-19 18:48:52 +0100

    Added new branch for doing general optimzation

--- main.cu
+++ main.cu
@@ -8,6 +8,7 @@
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
+#include "stdio.h"
 
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
@@ -69,6 +70,30 @@ __global__ void render_init(int max_x, int max_y, curandState *rand_state) {
     // performance improvement of about 2x!
     curand_init(1984+pixel_index, 0, 0, &rand_state[pixel_index]);
 }
+/*
+__global__ void generate_rays(ray* rays, int max_x, int max_y, int ns, curandState *rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
+    vec3 col(0,0,0);
+    for(int s=0; s < ns; s++) {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v, &local_rand_state);
+
+        float dx = rays[idx].direction.x();
+        float dy = rays[idx].direction.y();
+        float dz = rays[idx].direction.z();
+
+        // Compute bin for each axis
+        int bin_x = (dx > -9.5f) ? 1 : 0;
+        int bin_y = (dy > -9.5f) ? 1 : 0;
+        int bin_z = (dz > -9.5f) ? 1 : 0;
+
+    }
+} */
 
 __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -83,6 +108,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
         ray r = (*cam)->get_ray(u, v, &local_rand_state);
         col += color(r, world, &local_rand_state);
     }
+    printf("Direction: %0.2f \n", r.direction().x());
     rand_state[pixel_index] = local_rand_state;
     col /= float(ns);
     col[0] = sqrt(col[0]);
@@ -148,14 +174,17 @@ __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camer
 int main(int argc, char *argv[]) {
     int nx = 900;
     int ny = 600;
-    if(argc==3 && atoi(argv[1]) && atoi(argv[2])){
+    int print_image = 1;
+    if(argc==4 && atoi(argv[1]) && atoi(argv[2])){
       nx = atoi(argv[1]);
       ny = atoi(argv[2]);
+      print_image = atoi(argv[3]);
     }
     
     int ns = 10;
-    int tx = 8;
-    int ty = 8;
+    //New block dimensions
+    int tx = 16;
+    int ty = 32;
 
     std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel ";
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
@@ -206,14 +235,16 @@ int main(int argc, char *argv[]) {
     std::cerr << "took " << timer_seconds << " seconds.\n";
 
     // Output FB as Image
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            size_t pixel_index = j*nx + i;
-            int ir = int(255.99*fb[pixel_index].r());
-            int ig = int(255.99*fb[pixel_index].g());
-            int ib = int(255.99*fb[pixel_index].b());
-            std::cout << ir << " " << ig << " " << ib << "\n";
+    if (print_image) {
+        std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+        for (int j = ny-1; j >= 0; j--) {
+            for (int i = 0; i < nx; i++) {
+                size_t pixel_index = j*nx + i;
+                int ir = int(255.99*fb[pixel_index].r());
+                int ig = int(255.99*fb[pixel_index].g());
+                int ib = int(255.99*fb[pixel_index].b());
+                std::cout << ir << " " << ig << " " << ib << "\n";
+            }
         }
     }
 
