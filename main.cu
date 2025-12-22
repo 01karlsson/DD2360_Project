9104c41cb3c40a0785b8c8ec6fe649fa5901680f
Emil Karlsson <emilk3@kth.se>
2025-12-22 10:40:58 +0100

    mv float-to-8bit calc to gpu

--- main.cu
+++ main.cu
@@ -95,7 +95,7 @@ __global__ void generate_rays(ray* rays, int max_x, int max_y, int ns, curandSta
     }
 } */
 
-__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
+__global__ void render(uint8_t *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
@@ -108,13 +108,18 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
         ray r = (*cam)->get_ray(u, v, &local_rand_state);
         col += color(r, world, &local_rand_state);
     }
-    printf("Direction: %0.2f \n", r.direction().x());
+    //printf("Direction: %0.2f \n", r.direction().x());
     rand_state[pixel_index] = local_rand_state;
     col /= float(ns);
     col[0] = sqrt(col[0]);
     col[1] = sqrt(col[1]);
     col[2] = sqrt(col[2]);
-    fb[pixel_index] = col;
+    uint8_t red     = uint8_t(255.99*col[0]);
+    uint8_t green   = uint8_t(255.99*col[1]);
+    uint8_t blue    = uint8_t(255.99*col[2]);
+    fb[pixel_index*3]   = red;
+    fb[pixel_index*3+1] = green;
+    fb[pixel_index*3+2] = blue;
 }
 
 #define RND (curand_uniform(&local_rand_state))
@@ -190,10 +195,10 @@ int main(int argc, char *argv[]) {
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
     int num_pixels = nx*ny;
-    size_t fb_size = num_pixels*sizeof(vec3);
+    size_t fb_size = num_pixels*sizeof(uint8_t)*3;
 
     // allocate FB
-    vec3 *fb;
+    uint8_t *fb;
     checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
     // allocate random state
@@ -239,11 +244,11 @@ int main(int argc, char *argv[]) {
         std::cout << "P3\n" << nx << " " << ny << "\n255\n";
         for (int j = ny-1; j >= 0; j--) {
             for (int i = 0; i < nx; i++) {
-                size_t pixel_index = j*nx + i;
-                int ir = int(255.99*fb[pixel_index].r());
-                int ig = int(255.99*fb[pixel_index].g());
-                int ib = int(255.99*fb[pixel_index].b());
-                std::cout << ir << " " << ig << " " << ib << "\n";
+                size_t pixel_index = (j*nx + i)*3;
+                uint8_t ir = fb[pixel_index];   //int(255.99*fb[pixel_index].r());
+                uint8_t ig = fb[pixel_index+1]; //int(255.99*fb[pixel_index].g());
+                uint8_t ib = fb[pixel_index+2]; //int(255.99*fb[pixel_index].b());
+                std::cout << int(ir) << " " << int(ig) << " " << int(ib) << "\n";
             }
         }
     }
