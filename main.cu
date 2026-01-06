0b50e46ab4bf8d140f1d26ac2221608c48f91d30
AntonWallberg <awallber@kth.se>
2026-01-06 12:18:50 +0100

    restored main to original

--- main.cu
+++ main.cu
@@ -155,9 +155,9 @@ int main(int argc, char *argv[]) {
       print = atoi(argv[3]);
     }
     
-    int ns = 4;
-    int tx = 16;
-    int ty = 32;
+    int ns = 10;
+    int tx = 8;
+    int ty = 8;
 
     std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel ";
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
