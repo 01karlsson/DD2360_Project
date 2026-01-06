0e01d984f06e8163c5bdfefb531d29c0b7b56e51
AntonWallberg <awallber@kth.se>
2026-01-06 14:32:54 +0100

    changed *= in render sqrt

--- main.cu
+++ main.cu
@@ -85,9 +85,9 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
     }
     rand_state[pixel_index] = local_rand_state;
     col /= float(ns);
-    col[0] = col[0] * rsqrtf(col[0]);
-    col[1] = col[1] * rsqrtf(col[1]);
-    col[2] = col[2] * rsqrtf(col[2]);
+    col[0] *= rsqrtf(col[0]);
+    col[1] *= rsqrtf(col[1]);
+    col[2] *= rsqrtf(col[2]);
     fb[pixel_index] = col;
 }
 
